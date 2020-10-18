32755c47a836fa3cf92c65191ba748801caa6aea
Szczepan Abramczyk <szczepanab@gmail.com>
2020-10-18 21:03:23 +0200

    hamburger in work

---
+++ QuizApp-SPA/src/app/modules/start-page/components/hamburger/hamburger.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { HamburgerComponent } from './hamburger.component';
+
+describe('HamburgerComponent', () => {
+  let component: HamburgerComponent;
+  let fixture: ComponentFixture<HamburgerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HamburgerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HamburgerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
