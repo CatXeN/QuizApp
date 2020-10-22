80db294875057a866017d5cdb87a9bf88ede92e9
Kamil Kot <kamil.kot07@gmail.com>
2020-10-22 17:43:03 +0200

    Register view and service

--- QuizApp-SPA/src/app/modules/auth/auth-routing.module.ts
+++ QuizApp-SPA/src/app/modules/auth/auth-routing.module.ts
@@ -1,3 +1,4 @@
+import { RegisterComponent } from './components/register/register.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
--- QuizApp-SPA/src/app/modules/auth/auth.module.ts
+++ QuizApp-SPA/src/app/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
+import { RegisterComponent } from './components/register/register.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { CoreModule } from '../../core/layouts/core.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { MaterialModule } from 'src/app/material';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,8 +14,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     CoreModule,
     AuthRoutingModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, RegisterComponent]
 })
 export class AuthModule { }
