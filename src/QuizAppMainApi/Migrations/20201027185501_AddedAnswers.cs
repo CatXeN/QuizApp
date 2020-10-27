f174f57d573631b4c0253c6fc7c8f54902573658
TellMeAjgor <igorjedrzejek@gmail.com>
2020-10-27 19:57:45 +0100

    Added Answers

---
+++ src/QuizAppMainApi/Migrations/20201027185501_AddedAnswers.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuizAppMainApi.Migrations
+{
+    public partial class AddedAnswers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnswerA",
+                table: "Quests",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnswerB",
+                table: "Quests",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnswerC",
+                table: "Quests",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnswerD",
+                table: "Quests",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectAnswer",
+                table: "Quests",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswerA",
+                table: "Quests");
+
+            migrationBuilder.DropColumn(
+                name: "AnswerB",
+                table: "Quests");
+
+            migrationBuilder.DropColumn(
+                name: "AnswerC",
+                table: "Quests");
+
+            migrationBuilder.DropColumn(
+                name: "AnswerD",
+                table: "Quests");
+
+            migrationBuilder.DropColumn(
+                name: "CorrectAnswer",
+                table: "Quests");
+        }
+    }
+}
--- src/QuizAppModels/Models/Entities/Quest.cs
+++ src/QuizAppModels/Models/Entities/Quest.cs
@@ -12,7 +12,13 @@ namespace QuizAppModels.Models.Entities
         public int QuestId { get; set; }
         public string  Content { get; set; }
         public int Time { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+        public int CorrectAnswer { get; set; }
         public int QuizId { get; set; }
         public virtual Quiz Quiz { get; set; }
+
     }
 }
