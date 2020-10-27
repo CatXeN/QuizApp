76622eeed4be1053f6e9966c02b1517862527387
TellMeAjgor <igorjedrzejek@gmail.com>
2020-10-27 23:16:54 +0100

    AddedContent v2

--- src/QuizAppModels/Models/Entities/Quiz.cs
+++ src/QuizAppModels/Models/Entities/Quiz.cs
@@ -10,7 +10,6 @@ namespace QuizAppModels.Models.Entities
         [Key]
         public int QuizId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public int UserId { get; set; }
         public string ImageUrl { get; set; }
         public string Content { get; set; }
--- src/QuizAppModels/Models/Informations/QuizInformation.cs
+++ src/QuizAppModels/Models/Informations/QuizInformation.cs
@@ -9,7 +9,6 @@ namespace QuizAppModels.Models.Informations
     {
         public int QuizId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public string ImageUrl { get; set; }
         public string Content { get; set; }
         public int UserId { get; set; }
