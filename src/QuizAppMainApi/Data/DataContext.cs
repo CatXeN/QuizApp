bac569a02a6a1d843eadb1138eb73e93643dc3c0
Kamil Kot <kamil.kot07@gmail.com>
2020-11-18 20:47:05 +0100

    fix empty line

--- src/QuizAppMainApi/Data/DataContext.cs
+++ src/QuizAppMainApi/Data/DataContext.cs
@@ -11,7 +11,6 @@ namespace QuizAppMainApi.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options) { }
-
         public DbSet<Category> Categories { get; set; }
         public DbSet<Quest> Quests { get; set; }
         public DbSet<Quiz> Quizzes{ get; set; }
