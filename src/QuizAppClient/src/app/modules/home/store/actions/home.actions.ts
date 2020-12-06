bf5e51083af4456dba8d251df23ebd195450cc5e
Kamil Kot <kamil.kot07@gmail.com>
2020-12-06 19:16:19 +0100

    usuniecie potrzebnych rzeczy'

--- src/QuizAppClient/src/app/modules/home/store/actions/home.actions.ts
+++
@@ -1,5 +0,0 @@
-import { Quiz } from './../../../../shared/Models/quiz.model';
-import { createAction, props } from '@ngrx/store';
-
-export const LoadQuizzes = createAction('[Home Component] LoadQuizzes', props<{ quizzes: Quiz[]}>());
-export const LoadQuizzesFail = createAction('[Home Component] LoadQuizzesFail', props<{ message: string }>());
