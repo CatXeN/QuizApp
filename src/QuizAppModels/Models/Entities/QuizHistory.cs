1fb3ee0722432680241c27ad35e253e7e750eeb7
Unit67 <Unit67@interia.pl>
2020-12-08 21:58:26 +0100

    Added new table QuizHistories

---
+++ src/QuizAppModels/Models/Entities/QuizHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace QuizAppModels.Models.Entities
+{
+    public class QuizHistory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid QuizHistoryId { get; set; }
+        public Guid QuizId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid Score { get; set; }
+        public Guid Date { get; set; }
+    }
+}
---
+++ src/QuizAppModels/Models/Informations/QuizHistoryInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizAppModels.Models.Informations
+{
+    public class QuizHistoryInformation
+    {
+        public Guid QuizHistoryId { get; set; }
+        public Guid QuizId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid Score { get; set; }
+        public Guid Date { get; set; }
+    }
+}
