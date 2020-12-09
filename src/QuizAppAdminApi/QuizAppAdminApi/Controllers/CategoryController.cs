3cab311cbca3c1c6a3f1635a0909e300ea3f4749
Igor Jedrzejek <igorjedrzejek@gmail.com>
2020-12-09 21:58:53 +0100

    Added AdminApi microservice

---
+++ src/QuizAppAdminApi/QuizAppAdminApi/Controllers/CategoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizAppAdminApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
---
+++ src/QuizAppAdminApi/QuizAppAdminApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizAppModels.Models.Entities;
+
+namespace QuizAppAdminApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Quest> Quests { get; set; }
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<QuizHistory> QuizHistories { get; set; }
+    }
+}
